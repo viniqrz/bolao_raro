09ad714f6e64f2013584526e33f3b2d48d0a4dcd
Vinicius <viniderp@gmail.com>
2021-11-12 20:14:00 -0300

    axios compile fix

--- src/infra/http/AxiosHttpClient.ts
+++ src/infra/http/AxiosHttpClient.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "../../@types/infra/http/HttpClient";
 import axios, { Axios } from "axios";
 
 export class AxiosHttpClient extends Axios implements HttpClient {
-  constructor() {
-    super(axios.defaults);
-  }
+  // constructor() {
+  //   super(axios.defaults);
+  // }
 }
